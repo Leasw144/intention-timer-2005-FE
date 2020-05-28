d4a8b73991b8831061d7c18b4c5853f5aa89bfe0
Jeff Woltjen <jeff.woltjen@gmail.com>
2020-05-28 14:33:14 -0500

    add initial variables for buttons on main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+//~~~~~~~~~~~~~~~~~~'Control' class buttons~~~~~~~~~~~~~~~~~~~~~~
+var studyButton = document.querySelector()
+var meditateButton = document.querySelector()
+var exerciseButton = document.querySelector()
+//~~~~~~~~~~~~~~~~~~~'Sections'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+var homeSection = document. querySelector()
+var currentActivitySection = document.querySelector()
+var completedActivitySection = document.querySelector()
+//~~~~~~~~~~~~~~~'Event Listeners'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+selectStudyButton.addEventListener('click', goToCurrentActivitySection)
+//~~~~~~~~~~~~~~~'Event Handlers'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function goToCurrentActivitySection () {
+  currentActivity = new Activity(
+    this.category = category;
+    this.description = description;
+    this.minutes = minutes;
+    this.seconds = seconds;
+    this.completed = false;
+    this.id = id;
+  )
+  startTimer(){
+
+  }
+  markComplete(){
+
+  }
+  saveToStorage(){
+    
+  }
+}
