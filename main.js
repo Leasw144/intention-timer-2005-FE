509ace9bfbf2ec10c0ce80863500ee0c07d522fe
Jeff Woltjen <jeff.woltjen@gmail.com>
2020-05-30 14:57:44 -0500

    Add functionality to start activity button where clicking button will switch to CurrentactivityPage

--- main.js
+++ main.js
@@ -7,41 +7,35 @@ var startActivityButton = document.querySelector('.start-btn')
 var homePage = document.querySelector('.main-page')
 var currentActivityPage = document.querySelector('.currentActivity-page')
 var completedActivityPage = document.querySelector('.completedActivity-page')
-//add hidden sections somewhere like in ROMCOM//
-//var hidden = document.querySelector('.hidden')
+var hidden = document.querySelector('.hidden')
 //~~~~~~~~~~~~~~~'User Inputs'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 var currentGoal = document.querySelector('.goal')
 var currentMinutes = document.querySelector('.minutes')
 var currentSeconds = document.querySelector('.seconds')
 //~~~~~~~~~~~~~~~'Event Listeners'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-startActivityButton.addEventListener('click', goToCurrentActivity-page)
+startActivityButton.addEventListener('click', activateStartButton)
 //~~~~~~~~~~~~~~~'Event Handlers'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-/* this is for iteration 3
-function handleClick(event) {
-  if (event.target.className === '.study-btn') {
-    displayCurrentActivityPage();
-    displayStudyActivityProperties();
-  };
-  if (event.target.className === '.meditate-btn'){
-    displayCurrentActivityPage();
-    displayMeditateActivityProperties();
-  };
-  if (event.target.className === '.exercise-btn'){
-    displayCurrentActivityPage();
-    displayExerciseActivityProperties();
-  }
-  //if (condition === true && event.target.id === 'go-back-button'){
-    //displayPreviousPage();
-  }
-*/
+function activateStartButton(){
+  hideHomePage();
+  createNewActivity();
+  console.log('this is working')
+}
+
 //~~~~~~~~~~The one source of truth~~~~~~~~~~~~~~~ /
 var currentActivity
 var completedActivities
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
-
+function createNewActivity(){
+  currentActivity = new Activity
+}
 /* handling input from forms in ROMCOM, could be useful for Activity function
 function displayMyCover() {
   coverImage.src = userCoverInput.value
   return coverImage.src;
 */
+//~~~~~~~~~~~Navigation~~~~~~~~~~~~~~~~~~~~~~~//
+function hideHomePage(){
+  homePage.classList.add('hidden')
+  currentActivityPage.classList.remove('hidden')
+}
