30a9664c5a5f3d6eda231ce0fb416b119f37a07f
Jeff Woltjen <jeff.woltjen@gmail.com>
2020-05-31 15:00:27 -0500

    continue working on activities.js

--- activity.js
+++ activity.js
@@ -17,26 +17,27 @@ class Activity {
       counter++;
       console.log(timeLeft-counter)
       timer.innerHTML = (timeLeft-counter)
+      if ((timeLeft-counter) <= 0) {
+        return ("Congratulations!")
+      }
     }
     setInterval(timeIt, 1000);
   }
 
   markComplete(){
-    if timer.innerHTML =< 0{
-      alert("Congratulations!")
+      alert("Congratulations!");
       this.completed = true;
     }
-  }
 
   saveToStorage(){
     if (this.completed = true){
     completedActivities.push(currentActivity);
     for (let i = 0; i < completedActivities.length; i++){
-    completedActivities.[i].innerHTML = <div class="logged-card">
-      <h3 class="category-card-title meditate">Meditate</h3>
-      <p class="logged-time"> 5 MINUTES 0 SECONDS </p>
-      <p class="logged-activity">Deep Breathing</p>
-    </div>
+    completedActivities.innerHTML = `<div class="logged-card">
+      <h3 class="category-card-title ${this.category}"></h3>
+      <p class="logged-time"> ${this.minutes} min : ${this.seconds} sec </p>
+      <p class="logged-activity">${this.description}</p>
+    </div>`
       }
     }
   }
--- main.js
+++ main.js
@@ -50,14 +50,14 @@ function colorMeditateBtn(event) {
 
 //~~~~~~~~~~~~~~~'Event Handlers'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 function activateStartButton(){
-  if (currentGoal !== null && currentMinutes !== null && currentSeconds !== null && (studyButton.innerHTML === `<img src="./assets/study-active.svg"/><h4 class="study">Study</h4>` || exerciseButton.innerHTML === `<img src="./assets/exercise-active.svg"/> <h4 class="exercise">Exercise</h4>` || meditateButton.innerHTML ===`<img src="./assets/meditate-active.svg"/> <h4 class="meditate">Meditate</h4>`)){
+//  if (currentGoal !== null && currentMinutes !== null && currentSeconds !== null && (studyButton.innerHTML === `<img src="./assets/study-active.svg"/><h4 class="study">Study</h4>` || exerciseButton.innerHTML === `<img src="./assets/exercise-active.svg"/> <h4 class="exercise">Exercise</h4>` || meditateButton.innerHTML ===`<img src="./assets/meditate-active.svg"/> <h4 class="meditate">Meditate</h4>`)){
   createNewActivity();
   renderCurrentActivity();
   console.log('this is working')
-} else {
-  alert ('You need to complete all four forms before continuing!')
+//} else {
+  //alert ('You need to complete all four forms before continuing!')
 }
-}
+
 /* this is for iteration 3
 function handleClick(event) {
   if (event.target.className === '.study-btn') {
@@ -80,7 +80,7 @@ function handleClick(event) {
 
 //~~~~~~~~~~The one source of truth~~~~~~~~~~~~~~~ /
 var currentActivity
-var completedActivities = []; 
+var completedActivities = [];
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 function createNewActivity() {
   currentActivity = new Activity (
